2e976db7cb4500a84f10d07fe6fcd8d5b0f233f9
Diya <pdiya2945@gmail.com>
2025-06-07 18:09:03 +0530

    learn about hooks

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter = 5;
+  let [counter , setCounter] = useState(0)
+
+  const addValue = () => {
+    console.log("clikcked", counter)
+    if(counter<=19)
+    {
+      counter = counter+1 
+      setCounter(counter)
+    }
+  }
+
+  const removeValue = () => {
+    console.log("clicked", counter)
+    if(counter>=1)
+    {
+      counter = counter-1
+      setCounter(counter)
+    }
+    
+  }
+  let [user,setUser] = useState({
+      name : "Diya",
+      age : 19,
+      city : "Nadiad",
+    });
+    const [nameInput , setNameInput] = useState("");
+    const [cityInput, setCityInput] = useState("");
+
+    const changeName = () => {
+      setUser({
+        ...user,
+        name : nameInput,
+      });
+      setNameInput("");   // Clear the input field after update (optional)
+    }
+
+    const increaseAge = () =>
+    {
+      setUser({
+        ...user,
+        // age : age+1    //You're calculating a new value based on the current age, 
+                          // so you must write user.age + 1.               
+        age : user.age + 1
+      });
+    };
+
+    const changeCity = () => {
+    setUser({
+      ...user,
+      city: cityInput
+    });
+    setCityInput(""); // Clear the input field after update (optional)
+  };
+  
+
+  return (
+    <>
+      <div>
+        <h1>User Profile</h1>
+        <p><strong>Name: </strong>{user.name}</p>
+        <p><strong>Age:</strong> {user.age}</p>
+        <p><strong>City:</strong> {user.city}</p>
+
+        <input type="text" placeholder="Enter New Name" value={nameInput}
+        onChange={(e) => setNameInput(e.target.value)} />
+        <button onClick={changeName}>Change Name</button>
+        <br/> <br/>
+
+        <button onClick={increaseAge}>Increase Age</button>
+        <br /><br />
+
+        <input type="text" placeholder="Enter new city" value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)} />  
+
+        {/* e is the event object (automatically passed when the user types).
+            e.target refers to the input field itself.
+            e.target.value is the current text inside the input field.
+            setCityInput(...) is called to update the React state (cityInput) with the new value.
+        */}
+        <button onClick={changeCity}>Change City</button>
+        
+      </div>
+
+      <div>
+        <h1>Counter With React</h1>
+        <h2>Counter value: {counter}</h2>
+
+        <button onClick={addValue}>Increase Value</button>
+        <br />
+        <button onClick={removeValue}>Decrese Value</button>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
